593e5f918c55d3a391c32d6c4ce7641c8060afaf
Dmitry Plotnikov <dmplotn@gmail.com>
2021-01-31 11:08:58 +0000

    add greeting

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $playerName = prompt('May I have your name?');
+    line("Hello, {$playerName}!");
+}
